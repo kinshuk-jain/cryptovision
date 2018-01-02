defc9c7aee378335d902910a8c208534e602f5e5
kinshuk <kinshuk@sumologic.com>
2018-01-02 14:29:39 +0530

    adding share options to question and answers

--- src/routes/questionPage/QuestionPage.js
+++ src/routes/questionPage/QuestionPage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import cx from 'classnames';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './QuestionPage.css';
 
@@ -17,75 +18,104 @@ class QuestionPage extends React.Component {
     title: PropTypes.string.isRequired,
   };
 
+  renderVoteContainer() {
+    return (
+      <ul className={cx(s.quesStats, s.voteContainer)} key={Math.random()}>
+        <li>
+          <i className="icon-arrow-up" /> Upvote
+        </li>
+        <li>
+          <i className="icon-arrow-down" /> Downvote
+        </li>
+      </ul>
+    );
+  }
+
+  renderAnswer() {
+    return (
+      <div className={s.answerContainer}>
+        <hr />
+        <div className={s.answerCaption}>
+          Written by abcd@gmail.com on 23rd June, 2017
+        </div>
+        <ul className={s.shareOptions}>
+          <li>
+            <i className="icon-facebook-official" />
+          </li>
+          <li>
+            <i className="icon-twitter-square" />
+          </li>
+          <li>
+            <i className="icon-flag" />
+          </li>
+        </ul>
+        <div className={s.answerTitle}>It is a cryptocurrency</div>
+        <div className={s.answer}>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry&apos;s standard dummy
+            text ever since the 1500s, when an unknown printer took a galley of
+            type and scrambled it to make a type specimen book. It has survived
+            not only five centuries, but also the leap into electronic
+            typesetting, remaining essentially unchanged. It was popularised in
+            the 1960s with the release of Letraset sheets containing Lorem Ipsum
+            passages, and more recently with desktop publishing software like
+            Aldus PageMaker including versions of Lorem Ipsum.
+          </p>
+        </div>
+        {this.renderVoteContainer()}
+        <Comments />
+      </div>
+    );
+  }
+
+  renderQuestion() {
+    return [
+      <h4 key="title">{this.props.title}</h4>,
+      <div className={s.options} key="useroptions">
+        <ul className={s.quesStats}>
+          <li>123 Anwers</li>
+          <li>13.4k Upvotes</li>
+          <li>456 Downvotes</li>
+        </ul>
+        <ul className={s.shareOptions}>
+          <li>
+            <i className="icon-facebook-official" />
+          </li>
+          <li>
+            <i className="icon-twitter-square" />
+          </li>
+          <li>
+            <i className="icon-flag" />
+          </li>
+        </ul>
+      </div>,
+      <hr key="hr" />,
+      <p key="description">
+        description: lorum ipsum textLorem Ipsum is simply dummy text of the
+        printing and typesetting industry. Lorem Ipsum has been the
+        industry&apos;s standard dummy text ever since the 1500s, when an
+        unknown printer took a galley of type and scrambled it to make a type
+        specimen book. It has survived not only five centuries, but also the
+        leap into electronic typesetting, remaining essentially unchanged. It
+        was popularised in the 1960s with the release of Letraset sheets
+        containing Lorem Ipsum passages, and more recently with desktop
+        publishing software like Aldus PageMaker including versions of Lorem
+        Ipsum.
+      </p>,
+      this.renderVoteContainer(),
+      <Comments key="comments" />,
+    ];
+  }
+
   render() {
     return (
       <div className={s.quesContainer}>
         <div className={s.leftContainer}>
           <Tags tags={['bitcoin', 'crypto', 'ico']} />
-          <h4>{this.props.title}</h4>
-          <div className={s.options}>
-            <ul className={s.quesStats}>
-              <li>123 Anwers</li>
-              <li>13.4k Upvotes</li>
-              <li>456 Downvotes</li>
-            </ul>
-            <ul className={s.shareOptions}>
-              <li>
-                <i className="icon-facebook-official" />
-              </li>
-              <li>
-                <i className="icon-twitter-square" />
-              </li>
-              <li>
-                <i className="icon-flag" />
-              </li>
-            </ul>
-          </div>
-          <hr />
-          <p>
-            description: lorum ipsum textLorem Ipsum is simply dummy text of the
-            printing and typesetting industry. Lorem Ipsum has been the
-            industry&apos;s standard dummy text ever since the 1500s, when an
-            unknown printer took a galley of type and scrambled it to make a
-            type specimen book. It has survived not only five centuries, but
-            also the leap into electronic typesetting, remaining essentially
-            unchanged. It was popularised in the 1960s with the release of
-            Letraset sheets containing Lorem Ipsum passages, and more recently
-            with desktop publishing software like Aldus PageMaker including
-            versions of Lorem Ipsum.
-          </p>
-          <Comments />
-          <div className={s.answerContainer}>
-            <hr />
-            <div className={s.answerCaption}>
-              Written by abcd@gmail.com on 23rd June, 2017
-            </div>
-            <ul className={s.shareOptions}>
-              <li>
-                <i className="icon-facebook-official" />
-              </li>
-              <li>
-                <i className="icon-twitter-square" />
-              </li>
-              <li>
-                <i className="icon-flag" />
-              </li>
-            </ul>
-            <div className={s.answerTitle}>It is a cryptocurrency</div>
-            <div className={s.answer}>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry&apos;s standard dummy
-              text ever since the 1500s, when an unknown printer took a galley
-              of type and scrambled it to make a type specimen book. It has
-              survived not only five centuries, but also the leap into
-              electronic typesetting, remaining essentially unchanged. It was
-              popularised in the 1960s with the release of Letraset sheets
-              containing Lorem Ipsum passages, and more recently with desktop
-              publishing software like Aldus PageMaker including versions of
-              Lorem Ipsum.
-            </div>
-            <Comments />
-          </div>
+          {this.renderQuestion()}
+          {this.renderAnswer()}
+          {this.renderAnswer()}
           <div>Answer Question</div>
         </div>
         <div className={s.rightContainer}>
